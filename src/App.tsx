b91c25a2eaef2fc071efc2cd21b7a31997943130
jecsham <jecsham27@gmail.com>
2021-06-11 15:00:02 -0500

    add privacy policy for epic monitor

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,46 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Container } from 'react-bootstrap';
-import Hero from './components/Hero';
-import Projects from './components/Projects';
-import OtherProjects from './components/OtherProjects';
-import Courses from './components/Courses';
-import Contributions from './components/Contributions';
+import React, { Component } from "react";
+import "./App.css";
+import { Container } from "react-bootstrap";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import OtherProjects from "./components/OtherProjects";
+import Courses from "./components/Courses";
+import Contributions from "./components/Contributions";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import EMPrivacyPolicy from "./components/EMPrivacyPolicy";
 
 interface MyProps {
-  value?: any
+  value?: any;
 }
 interface MyState {
   value?: any;
 }
 
 class App extends Component<MyProps, MyState> {
-
   constructor(props: any) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
 
   render() {
     return (
-      <Container>
-        <Hero />
-        <Projects />
-        <OtherProjects />
-        <Courses />
-        <Contributions />
-      </Container>
+      <Router>
+            <Container>
+        <Switch>
+          <Route path="/em-privacy-policy">
+            <EMPrivacyPolicy />
+          </Route>
+          <Route path="/">
+              <Hero />
+              <Projects />
+              <OtherProjects />
+              <Courses />
+              <Contributions />
+          </Route>
+        </Switch>
+            </Container>
+      </Router>
     );
-
   }
 }
 
