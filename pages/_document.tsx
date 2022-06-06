83239c78fb9a0612fa7432c72ef1671499a04b70
Jecsham <jecsham27@gmail.com>
2022-06-06 18:43:10 -0500

    improved Lighthouse

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html>
       <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="description"
+          content="Jecsham Castillo - Full Stack Developer"
+        />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -13,6 +22,22 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap"
           rel="stylesheet"
+        />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-130625337-1"
+        />
+        <Script
+          id="gtmscript"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+        gtag('config', 'UA-130625337-1');
+        `,
+          }}
         />
       </Head>
       <body>
